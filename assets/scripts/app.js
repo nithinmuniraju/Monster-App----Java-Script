5420cc0f2da7c906ded80c0954dbb46cb194baf8
NithinM <nithinmuniraju@gmail.com>
2020-09-27 17:43:46 +0530

    Added Heal, Reset, Bonuslife. Also added function reusability

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,35 +1,64 @@
 const MONSTER_ATTACK = 14;
 const PLAYER_NORMAL_ATTACK = 10;
 const PLAYER_STRONG_ATTACK = 16;
+const PLAYER_HEAL_VALUE = 20;
 
-const DefaultHealth = 100;
+let setHealth = prompt('Set initial Health','100'); // Convert this into a model on LOAD
+
+if(!setHealth || setHealth <= 0 ){
+    setHealth = 100;
+}
+const DefaultHealth = setHealth;
+// const DefaultHealth = 100;
 let initialMonsterHealth = DefaultHealth;
 let initialPlayerHealth = DefaultHealth;
+let hasBonus = true;
  
 adjustHealthBars(DefaultHealth);
 
+// Reset function on Game Over
+function reset(){
+    initialMonsterHealth = DefaultHealth;
+    initialPlayerHealth = DefaultHealth;
+    resetGame(DefaultHealth);
+}
+
+function endRound(){
+    const getBonusHealth = initialPlayerHealth;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK);
+    initialPlayerHealth -= playerDamage
+
+    if(initialPlayerHealth <= 0 && hasBonus){
+        hasBonus = false;
+        removeBonusLife();
+        initialPlayerHealth = getBonusHealth;
+        setPlayerHealth(getBonusHealth);
+        alert('You would be dead but the bonus life saved you');
+    }
+
+    if(initialMonsterHealth <= 0 && initialPlayerHealth > 0){
+        alert('PLAYER Won!!');
+        reset()
+    } else if(initialPlayerHealth <= 0 && initialMonsterHealth > 0){
+        alert('PLAYER Lost :(');
+        reset()
+    } else if(initialPlayerHealth <= 0 && initialMonsterHealth <= 0){
+        alert('Match Draw');
+        reset()
+    }
+}
 function attackMode(mode){
+    console.log('mode',mode);
     let attackType;
     if(mode === "NORMAL"){
         attackType = PLAYER_NORMAL_ATTACK;
     } else if(mode === "STRONG"){
         attackType = PLAYER_STRONG_ATTACK;
     }
-
     const moanstrDamage = dealMonsterDamage(attackType);
     initialMonsterHealth -= moanstrDamage
 
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK);
-    initialPlayerHealth -= playerDamage
-    console.log('playerDamage',initialPlayerHealth);
-
-    if(initialMonsterHealth <= 0 && initialPlayerHealth > 0){
-        alert('PLAYER Won!!');
-    } else if(initialPlayerHealth <= 0 && initialMonsterHealth > 0){
-        alert('PLAYER Lost :(');
-    } else if(initialPlayerHealth <= 0 && initialMonsterHealth <= 0){
-        alert('Match Draw');
-    }
+    endRound();
 }
 
 function attackHandeler(){
@@ -44,7 +73,25 @@ function strongAttackHandeler(){
     attackMode('STRONG');
 }
 
+function healHandeler(){
+    let healValue;
+
+    if(initialPlayerHealth >= DefaultHealth - PLAYER_HEAL_VALUE){
+        alert('You can\'t heal more than your max initial health');
+        healValue = DefaultHealth - initialPlayerHealth;
+    } else {
+        healValue = PLAYER_HEAL_VALUE
+    }
+
+    increasePlayerHealth(PLAYER_HEAL_VALUE);
+    initialPlayerHealth += healValue;
+
+   endRound();
+}
+
 //Add a Click functionality for Attack
 attackBtn.addEventListener('click', attackHandeler);
 //Add a Click functionality for Strong Attack
 strongAttackBtn.addEventListener('click', strongAttackHandeler);
+//Add a Click functionality for Heal
+healBtn.addEventListener('click', healHandeler);
